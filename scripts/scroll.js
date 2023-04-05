1095a77ba7b3aff388f3f9adf29d99893643cf4b
vladyslav.ralets <vladyslav.ralets@tacr-external.cz>
2023-04-05 12:36:33 +0200

    added careers

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -7,3 +7,21 @@ logo.addEventListener('click', () => {
     behavior: 'smooth'
   });
 });
+
+// Accordion
+
+const titles = document.querySelectorAll('.career__title');
+const descriptions = document.querySelectorAll('.career__description');
+
+titles.forEach((title, index) => {
+  title.addEventListener('click', () => {
+    alert(index, title);
+    // Скрыть текущий открытый элемент гармошки
+    const currentDescription = document.querySelector('.career__description.show');
+    if (currentDescription && currentDescription !== descriptions[index]) {
+      currentDescription.classList.remove('show');
+    }
+    // Показать выбранный элемент гармошки
+    descriptions[index].classList.toggle('show');
+  });
+});
